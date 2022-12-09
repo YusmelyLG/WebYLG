ae2d8809f95281bbfa7e0e60421bcd293e44f323
YusmelyLG <yusmelylg@gmail.com>
2022-12-09 17:51:30 -0330

    js

---
+++ web.js
@@ -0,0 +1,35 @@
+//Toggle menu
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+function toggleMenu() {
+    if (menu.classList.contains("active")){
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+    }
+
+toggle.addEventListener('click',toggleMenu, false);
+
+//Submenu
+const item = document.querySelector('.item');
+
+function toggleItem() {
+    if (this.classList.contains("submenu-active")){
+        this.classList.remove("submenu-active");
+    } else {
+        menu.querySelector("submenu-active").classList.remove("submenu-active"); this.classList.add("submenu-active");
+    } else {
+        this.classList.add("submenu-active");
+    }
+}
+
+for (let item of items){
+    if (item.querySelector(".submenu")){
+        item.addEventListener('click', toggleItem, flase);
+        item.addEventListener('keypress', toggleItem, flase);
+    }
+}
