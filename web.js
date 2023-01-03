682b15af5b671b9191427e01c71621684a0049d1
YusmelyLG <yusmelylg@gmail.com>
2023-01-03 04:58:06 -0330

    nuevos cambios

--- web.js
+++ web.js
@@ -1,115 +1,18 @@
-//Toggle menu
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
- 
-/* Toggle menu */
-function toggleMenu() {
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-         
-        // adds the menu (toggle) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
-    } else {
-        menu.classList.add("active");
-         
-        // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
-    }
-}
-function toggleMenu() {
-  if (menu.classList.contains("clicked")) {
-      menu.classList.remove("clicked");
-       
-      // adds the menu (toggle) icon
-      toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
-  } else {
-      menu.classList.add("active");
-       
-      // adds the close (x) icon
-      toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
-  }
-}
-/* Event Listener */
+//Dropdown menu/
+document.addEventListener('click',e => {
+   const isDropdownButton = e.target.matches('[data-dropdown-button]') 
+   if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
 
-/* Activate Dropdown */
+   let currentDropdown
+   if (!isDropdownButton){
+    currentDropdown = e.target.closest ('[data-dropdown]') 
+    currentDropdown.classList.toggle ('active')
+   } 
+   document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown)return
+    dropdown.classList.remove ('active')
+   })
+})
 
-     
-/* Close Dropdown From Anywhere */
 
-/* ContactMe Form*/
-<?php
-if (isset($_POST['Email'])) {
 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
-
-    function problem($error)
-    {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
-        echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
-        die();
-    }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['Name']) ||
-        !isset($_POST['Email']) ||
-        !isset($_POST['Message'])
-    ) {
-        problem('We are sorry, but there appears to be a problem with the form you submitted.');
-    }
-
-    $name = $_POST['Name']; // required
-    $email = $_POST['Email']; // required
-    $message = $_POST['Message']; // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The Name you entered does not appear to be valid.<br>';
-    }
-
-    if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
-    }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
-
-    // create email headers
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
-?>
-
-    <!-- include your success message below -->
-
-    Thank you for contacting us. We will be in touch with you very soon.
-
-<?php
-}
-?>
