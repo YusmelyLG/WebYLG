b9a9e4be64c9c55bd9667e74dcf450bfa2bda9ac
YusmelyLG <yusmelylg@gmail.com>
2023-01-03 12:50:07 -0330

    cambios

--- web.js
+++ web.js
@@ -1,8 +1,16 @@
-//Dropdown menu/
-$(document).ready(function(){
-   $(".dropdown-toggle").dropdown();
-});
-
-
+document.addEventListener ('click', e => {
+   const isDropdownMenu = e.target.matches("[data-dropdown-button]") 
+   if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
+   
+   let currentDropdown
+   if (isDropdownButton){
+      currentDropdown = e.target.closest('[data-dropdown')
+      currentDropdown.classlist.toggle('active')
+   }
+   document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+      if (dropdown === currentDropdown) return
+      dropdown.classlist.remove('active')
+   })
+})
 
 
