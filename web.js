424a6b4c857728f01f1cca840001e7e6f3c0f5ec
YusmelyLG <yusmelylg@gmail.com>
2023-01-04 14:16:22 -0330

    update

--- web.js
+++ web.js
@@ -1,15 +1,34 @@
-document.addEventListener ('click', e => {
-   if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
-   
-   let currentDropdown
-   if (isDropdownButton){
-      currentDropdown = e.target.closest('[data-dropdown')
-      currentDropdown.classlist.toggle('active')
+let toggleNavStatus = false;
+
+let toggleNav =function (){
+   let getSidebar = document.querySelector(".nav-sidebar");
+   let getSidebarUl = document.querySelector(".nav-sidebar ul");
+   let getSidebarTittle = document.querySelector(".nav-sidebar span");
+   let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+   if (toggleNavStatus === false) {
+      getSidebarUl.style.visibility = "visible";
+      getSidebarUl.style.width = "272px";
+      getSidebarUl.style.opacity = "0.5";
+
+      let arrayLength = getSidebarLinks.length;
+      for (let i =0; i <array.length; i++){
+         getSidebarLinks[i].style.opacity = "1";
+      }
+
+      toggleNavStatus = true;
    }
-   document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-      if (dropdown === currentDropdown) return
-      dropdown.classlist.remove('active')
-   })
-})
 
+   else if (toggleNavStatus === true) {
+      getSidebarUl.style.width = "50px";
+      getSidebarUl.style.opacity = "0";
 
+      let arrayLength = getSidebarLinks.length;
+      for (let i =0; i <array.length; i++){
+         getSidebarLinks[i].style.opacity = "0";
+      }
+      getSidebarUl.style.visibility = "hidden";
+
+      toggleNavStatus = false;
+   }
+}
