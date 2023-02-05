88183af1498c9e360d2dddcf57a4057ca335017f
YusmelyLG <yusmelylg@gmail.com>
2023-02-04 23:20:55 -0330

    new code

--- web.js
+++ web.js
@@ -1,3 +1,36 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+function openToggleMenu() {
+  let x = document.getElementById("myTopnav"); //this line grabs the element by its name
+    if (x.className === "topnav") { //set condition
+      x.className += "responsive"; //
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+function openDropdown() {
+    let x = document.getElementById("dropdown"); //this line grabs the element by its name
+      if (x.className === "dropdown") { //set condition
+        x.className += "responsive"; 
+      } else {
+        x.className = "topnav";
+      }
+    }
+  
+    function openDropdown() {
+      let x = document.getElementById("dropdown"); //this line grabs the element by its name
+        console.log 'item-c'
+      }
+      
+let language ={
+  eng:{
+    hi:"welcome everyone"
+  }
+  es:{
+    hi:"bienveneidos todos"
+  }
+}
+     
